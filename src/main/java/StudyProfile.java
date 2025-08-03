d08cec6d90a05cc92259da43fd32368b61e5a65b
royal <royalhellt@gmail.com>
2025-08-03 13:16:05 +0500

    Пункты 1-4
    Добавлены зависимости, созданы классы, создан enum с параметром

---
+++ src/main/java/StudyProfile.java
@@ -0,0 +1,20 @@
+public enum StudyProfile {
+    ECONOMICS("Экономика"),
+    PSYCHOLOGY("Психология"),
+    MEDICINE("Медицина"),
+    MATH("Математика"),
+    DESIGN("Дизайн"),
+    AVIATION("Авиация"),
+    JOURNALISM("Журналистика"),
+    ENGINEERING("Инженерия");
+
+    private final String profileName;
+
+    StudyProfile(String profileName) {
+        this.profileName = profileName;
+    }
+
+    public String getProfileName() {
+        return profileName;
+    }
+}
