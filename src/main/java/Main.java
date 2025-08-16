5e772da456d47bc1d83f368e61d50028ad5c3068
royal <royalhellt@gmail.com>
2025-08-16 16:36:34 +0500

    Пункт 11
    Выполнен сбор статистики и генерация Excel файла. Обновлен класс Main под задание 27.8.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,61 +8,8 @@ public class Main {
         List<Student> students = ExcelReader.studentReader();
         List<University> universities = ExcelReader.universityReader();
 
-        // Практическая неделя 3. Проект.
-
-        // пункт 4
-        String studentsJson = JsonUtil.serializeStudentList(students);
-        String universityJson = JsonUtil.serializeUniversityList(universities);
-
-        System.out.println(studentsJson);
-        System.out.println(universityJson + "\n");
-
-        // пункт 5
-        List<Student> deserializedStudents = JsonUtil.deserializeStudentList(studentsJson);
-        List<University> deserializedUniversities = JsonUtil.deserializeUniversityList(universityJson);
-
-        // пункт 6
-
-        if (students.size() == deserializedStudents.size()) {
-            System.out.printf("Количество студентов совпадает: %d\n", students.size());
-        }
-        else {
-            System.out.println(">>> STUDENTS НЕ СОВПАДАЮТ");
-        }
-
-        if (universities.size() == deserializedUniversities.size()) {
-            System.out.printf("Количество университетов совпадает: %d\n", universities.size());
-        }
-        else {
-            System.out.println(">>> UNIVERSITIES НЕ СОВПАДАЮТ");
-        }
-
-        // пункты 7-10
-
-        students.stream().forEach(student -> {
-            // пункт 7: сериализация
-            String json = JsonUtil.serializeStudent(student);
-
-            // пункт 8: вывод JSON-строки
-            System.out.println("Serialized JSON:\n" + json);
-
-            // пункт 9: десериализация
-            Student deserialized = JsonUtil.deserializeStudent(json);
-
-            // пункт 10: вывод обратно полученного объекта
-            System.out.println("Deserialized object:\n" + deserialized);
-        });
-
-        universities.stream().forEach(university -> {
-            String json = JsonUtil.serializeUniversity(university);
-
-            System.out.println("Serialized JSON:\n" + json);
-
-            University deserialized = JsonUtil.deserializeUniversity(json);
-
-            System.out.println("Deserialized object:\n" + deserialized);
-        });
-
-
+        // сбор статистики и генерация XLSX-файла
+        var statistics = StatisticsUtil.collectStatistics(students, universities);
+        XlsWriter.generateExcelStatistics(statistics, "statistics.xlsx");
     }
 }
